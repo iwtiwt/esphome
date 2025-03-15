ca7b06499e25d8f4f27093aa2d82e1168ec03c84
iwtiwt <111250859+iwtiwt@users.noreply.github.com>
2025-03-15 17:45:40 +0000

    Update sy6970.cpp

--- components/sy6970/sy6970.cpp
+++ components/sy6970/sy6970.cpp
@@ -47,29 +47,29 @@ void SY6970::reset_default() {
 void SY6970::enable_state_led() {
   ConfigurationRegister07 val;
   i2c::ErrorCode err = this->read_register(POWERS_PPM_REG_07H, &val.raw, 1);
-  ERROR_CHECK_RET(err);
+  ERROR_CHECK(err);
 
   val.status_pin_enable = 1;
   
   err =  this->write_register(POWERS_PPM_REG_07H, &val.raw, 1);
-  ERROR_CHECK_RET(err);
+  ERROR_CHECK(err);
 }
 
 void SY6970::disable_state_led() {
   ConfigurationRegister07 val;
   i2c::ErrorCode err = this->read_register(POWERS_PPM_REG_07H, &val.raw, 1);
-  ERROR_CHECK_RET(err);
+  ERROR_CHECK(err);
 
   val.status_pin_enable = 0;
   
   err = this->write_register(POWERS_PPM_REG_07H, val.raw, 1);
-  ERROR_CHECK_RET(err);
+  ERROR_CHECK(err);
 }
 
 void SY6970::disable_watchdog() {
 ConfigurationRegister07 val;
   i2c::ErrorCode err = this->read_register(POWERS_PPM_REG_07H, &val.raw, 1);
-  ERROR_CHECK_RET(err);
+  ERROR_CHECK(err);
 
   val.watchdog_timer = 0;
   
