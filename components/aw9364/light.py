c6837059d66ee33645571eb5cb12187155ab2329
iwtiwt <111250859+iwtiwt@users.noreply.github.com>
2025-12-16 02:42:35 +0000

    more

--- components/aw9364/light.py
+++ components/aw9364/light.py
@@ -9,14 +9,16 @@ AW9364 = aw9364_ns.class_("AW9364", light.LightOutput)
 CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
     {
         cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(AW9364),
-        cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput),
+        cv.Required(CONF_PIN): cv.All(cv.only_on_esp32, cv.uint8_t),
     }
-)
+).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
-    await light.register_light(var, config)
-
-    out = await cg.get_variable(config[CONF_OUTPUT])
-    cg.add(var.set_output(out))
+    # Set up the GPIO pin
+    pin = await cg.gpio_pin_expression(config[CONF_PIN])
+    cg.add(var.set_pin(pin))
+    # Set up the light
+    await light.setup_light(var, config)
+    await cg.register_component(var, config)
