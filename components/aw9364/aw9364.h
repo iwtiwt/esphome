45ac803099bee104b7bc7ab1a94761562f27c474
iwtiwt <111250859+iwtiwt@users.noreply.github.com>
2025-12-16 03:55:59 +0000

    x

--- components/aw9364/aw9364.h
+++ components/aw9364/aw9364.h
@@ -11,13 +11,11 @@ class AW9364 : public light::LightOutput, public Component {
  public:
   light::LightTraits get_traits() override;
   void write_state(light::LightState *state) override;
-  void set_output(output::FloatOutput *output) { output_ = output; }
   void set_output_pin(GPIOPin *pin) { this->pin_ = pin; }
 
   protected:
   GPIOPin *pin_;
   uint8_t current_brightness_;
-  output::FloatOutput *output_;
 
 };
 
--- components/aw9364/light.py
+++ components/aw9364/light.py
@@ -2,14 +2,13 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light, output
 from esphome import pins
-from esphome.const import CONF_OUTPUT, CONF_PIN
+from esphome.const import CONF_PIN
 
 aw9364_ns = cg.esphome_ns.namespace("aw9364")
 AW9364 = aw9364_ns.class_("AW9364", light.LightOutput, cg.Component)
 
 CONFIG_SCHEMA = light.BRIGHTNESS_ONLY_LIGHT_SCHEMA.extend(
     {
-        cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput),
         cv.Required(CONF_PIN): pins.gpio_output_pin_schema,
     }
 ).extend(cv.COMPONENT_SCHEMA)
@@ -19,6 +18,4 @@ async def to_code(config):
     var = await light.new_light(config)
     pin = await cg.gpio_pin_expression(config[CONF_PIN])
     cg.add(var.set_output_pin(pin))
-    out = await cg.get_variable(config[CONF_OUTPUT])
-    cg.add(var.set_output(out))
     await cg.register_component(var, config)
