b6be2c4b6bee2a307e0254aa73464b12474d54fd
iwtiwt <111250859+iwtiwt@users.noreply.github.com>
2025-12-16 02:46:24 +0000

    work

--- components/aw9364/aw9364.cpp
+++ components/aw9364/aw9364.cpp
@@ -8,33 +8,28 @@ static const char *TAG = "aw9364";
 
 
 void AW9364::write_state(light::LightState *state) override {
+  static uint8_t steps = 16;
   float brightness = state->current_values.get_brightness();
   uint8_t new_brightness = static_cast<uint8_t>(brightness * 16);
-  setBrightness(new_brightness);
-}
-
-void AW9364::setBrightness(uint8_t value)
-{
-    static uint8_t steps = 16;
-    if (value == 0) {
-        pin_->digital_write(false);
-        delay(3);
-        current_brightness_ = 0;
-        return;
-    }
-    if (current_brightness_ == 0) {
-        pin_->digital_write(true);
-        level = steps;
-        delayMicroseconds(30);
-    }
-    int from = steps - current_brightness_;
-    int to = steps - value;
-    int num = (steps + to - from) % steps;
-    for (int i = 0; i < num; i++) {
-        pin_->digital_write(false);
-        pin_->digital_write(true);
-    }
-    current_brightness_ = value;
+  if (new_brightness == 0) {
+      pin_->digital_write(false);
+      delay(3);
+      current_brightness_ = 0;
+      return;
+  }
+  if (current_brightness_ == 0) {
+      pin_->digital_write(true);
+      level = steps;
+      delayMicroseconds(30);
+  }
+  int from = steps - current_brightness_;
+  int to = steps - new_brightness;
+  int num = (steps + to - from) % steps;
+  for (int i = 0; i < num; i++) {
+      pin_->digital_write(false);
+      pin_->digital_write(true);
+  }
+  current_brightness_ = new_brightness;
 }
 
 
